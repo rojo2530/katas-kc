88b86d9d1f799922e8fc1e711fada0bbc748bb84
rojo2530 <jcantosg@gmail.com>
2019-07-03 10:44:01 +0200

    Limpio código y añado Arrow functions.

--- kata1/kata1.js
+++ kata1/kata1.js
@@ -1,56 +1,47 @@
+/* Verifica que el numero introducido esta en 1 y 100 */
 function testEntry (number) {
     return (number >=1 && number <=100);
 }
 
-// function digitsNumber (number) {
-//     let output = [];
-//     stringNumber = number.toString();
-//     for (let i = 0; i< stringNumber.length; i++) {
-//         output.push(parseInt(stringNumber[i]));
-//     }
-
-//     return output;
-
-// }
-
+/* Verifica que el numero es un entero y no un decimal */
 function digitsNumber (number) {
     return Array.from(number.toString(), Number);
 }
 
-
-
 function FooBarQuix (number) {
 
     if (!testEntry(number)) {
-        return 'El numero introducido no es correcto, tiene que estar entre 1 y 100';
+        return 'El numero ' + number +  ' introducido no es correcto, tiene que estar entre 1 y 100';
     }
 
     if (!Number.isInteger(number)) {
-        return 'El numero pasado no es un entero';
+        return 'El numero ' + number + ' pasado no es un entero';
     }
 
     let output = '';
     let arrayNumber = digitsNumber(number);
-    console.log(arrayNumber);
     const numberRelationString = {
         3: 'Foo',
         5: 'Bar',
         7: 'Quix'
     };
 
-    Object.keys(numberRelationString).forEach(function(element) {
+    Object.keys(numberRelationString).forEach((element) => {
         if (number % element == 0) {
             output += numberRelationString[element]; 
         }
     });
 
-    arrayNumber.forEach(function(element) {
+    arrayNumber.forEach((element) => {
         if (element in numberRelationString) {
             output += numberRelationString[element];
         }
     });
     
+    output = (output === '' ? number : output);
     return (number + ' -> ' + output);
 }
 
-console.log(FooBarQuix(21));
+for (let i = 0; i<= 100; i++) {
+    console.log(FooBarQuix(i));
+}
